6394b8f07092a55fe23997810650812c40e4d208
danninon <Danninon@gmail.com>
2024-08-09 12:36:59 +0300

    Added a health check controller, it check for extremely basic logic for validity of the mongodb

---
+++ Backend/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Database;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Backend.Controllers
+{
+    public class HealthController : Controller
+    {
+        private readonly IDbClient _dbClient;
+        public HealthController(IDbClient dbClient)
+        {
+            _dbClient = dbClient;
+        }
+
+        [HttpGet("health/mongo")]
+        public async Task<IActionResult> CheckMongoHealth()
+        {
+            try
+            {
+                var collection = _dbClient.GetCardsCollection();
+                await collection.Find(_ => true).Limit(1).ToListAsync(); // A simple query to check connection
+                return Ok("MongoDB is healthy.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"MongoDB health check failed: {ex.Message}");
+            }
+        }
+    }
+}
